d09c3fa54552c035626197e0f3ae6d60402ccde3
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2017-12-01 23:09:27 -0800

    adds signup and login

--- server/src/graphql/resolvers/tweet-resolvers.js
+++ server/src/graphql/resolvers/tweet-resolvers.js
@@ -4,15 +4,16 @@ export default {
   getTweet: (_, { _id }) => Tweet.findById(_id),
   getTweets: () => Tweet.find({}).sort({ createdAt: -1 }),
   createTweet: (_, args) => Tweet.create(args),
-  updateTweet: (_, { _id, ...rest }) => Tweet.findByIdAndUpdate(_id, rest, { new: true }),
+  updateTweet: (_, { _id, ...rest }) =>
+    Tweet.findByIdAndUpdate(_id, rest, { new: true }),
   deleteTweet: async (_, { _id }) => {
     try {
       await Tweet.findByIdAndRemove(_id);
       return {
-        message: 'Delete Success!'
-      }
+        message: 'Delete Success!',
+      };
     } catch (error) {
       throw error;
     }
-  }
+  },
 };
